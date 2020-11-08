7e4f7e42feb050da157aaabfdbdec29e9e90650d
Josh Karges <joshkarges@gmail.com>
2020-11-08 01:22:25 -0800

    Deploy 1604827343126

--- precache-manifest.7f94a46cfa0fd1b80846de7e316586c6.js
+++ precache-manifest.7f94a46cfa0fd1b80846de7e316586c6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4f203b1bce2f40b1ef07c9dfe28b04a",
+    "revision": "e6ff609ce3b1fe7e9c70523c5d3fb5c8",
     "url": "/index.html"
   },
   {
-    "revision": "18ac60168f726d396a35",
-    "url": "/static/css/main.bb05284c.chunk.css"
+    "revision": "c19955fc465350638812",
+    "url": "/static/css/main.7e05bc39.chunk.css"
   },
   {
-    "revision": "64accfbaf2f2fb0a363d",
-    "url": "/static/js/2.0d70201c.chunk.js"
+    "revision": "ae93b277ec166bb539a5",
+    "url": "/static/js/2.eea97522.chunk.js"
   },
   {
-    "revision": "43d72dab81fe6139b99e36ca2e0cb538",
-    "url": "/static/js/2.0d70201c.chunk.js.LICENSE.txt"
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.eea97522.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "18ac60168f726d396a35",
-    "url": "/static/js/main.ae918359.chunk.js"
+    "revision": "c19955fc465350638812",
+    "url": "/static/js/main.080a0586.chunk.js"
   },
   {
     "revision": "739694a59e9d5a99c62e",
@@ -36,10 +36,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/AngelicBonquesScript.68ab2671.otf"
   },
   {
-    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
-    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
-  },
-  {
     "revision": "be76c23aa3fce799130095342ab3fd97",
     "url": "/static/media/aboutPage1.be76c23a.png"
   },
@@ -48,16 +44,44 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/contactPage1.f9be08b4.png"
   },
   {
-    "revision": "4c9202acb40714fbd14f59a8ac7154cb",
-    "url": "/static/media/facebookLogo.4c9202ac.svg"
-  },
-  {
-    "revision": "6a5db6b46511c7fc6f43f11fc64a7277",
-    "url": "/static/media/houzzLogo.6a5db6b4.svg"
+    "revision": "27d12a72eadbb500ad6e50b8f2176410",
+    "url": "/static/media/highCeiling1.27d12a72.png"
   },
   {
     "revision": "fe40dbb7c46a1dbf44d27dfea39c2936",
     "url": "/static/media/instagramLogo.fe40dbb7.svg"
+  },
+  {
+    "revision": "379b15a740c2e66e84b9972287552a6a",
+    "url": "/static/media/portfolio1.379b15a7.png"
+  },
+  {
+    "revision": "5c14f049eed26c241072251e56fae5a2",
+    "url": "/static/media/portfolio2.5c14f049.png"
+  },
+  {
+    "revision": "16126592b1d747b294face7bc513af37",
+    "url": "/static/media/portfolio3.16126592.png"
+  },
+  {
+    "revision": "8d9c1957734d6837d90db3b69f376c43",
+    "url": "/static/media/portfolio3d1.8d9c1957.png"
+  },
+  {
+    "revision": "931007661d07f80c027ac469600f2e31",
+    "url": "/static/media/portfolio3d2.93100766.png"
+  },
+  {
+    "revision": "3db780f5e0053f84c49d0d88c5a9272e",
+    "url": "/static/media/portfolio4.3db780f5.png"
+  },
+  {
+    "revision": "67a5b5d22fb38d267629d996a43b4d3e",
+    "url": "/static/media/portfolio5.67a5b5d2.png"
+  },
+  {
+    "revision": "17ea1e0156b51a11823620c636b208f3",
+    "url": "/static/media/portfolio6.17ea1e01.png"
   },
   {
     "revision": "27412d13380bf0db89fd244d7896b10f",
@@ -82,9 +106,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "932748bd48553c032b4d2cc7b7545a71",
     "url": "/static/media/tykargesLogo.932748bd.svg"
-  },
-  {
-    "revision": "eee4b5b690de7c538a46b378785a9a7c",
-    "url": "/static/media/yelpLogo.eee4b5b6.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2081fcccba8b59a74a42d5ee8bdd6448.js"
+  "/precache-manifest.7f94a46cfa0fd1b80846de7e316586c6.js"
 );
 
 self.addEventListener('message', (event) => {
