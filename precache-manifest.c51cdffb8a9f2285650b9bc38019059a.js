44484ed77adb409d4e83db617c0cee79795c21b6
Josh Karges <joshkarges@gmail.com>
2022-12-26 23:09:54 -0800

    Deploy 1672124982740

---
+++ precache-manifest.c51cdffb8a9f2285650b9bc38019059a.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aded30ab2c79eac55c5490fc0716b691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba7e6d2c4be24a43e081",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "c4a4dab399a20da78ee8",
+    "url": "/static/css/main.cd76ec18.chunk.css"
+  },
+  {
+    "revision": "ba7e6d2c4be24a43e081",
+    "url": "/static/js/2.94d9c751.chunk.js"
+  },
+  {
+    "revision": "70593bdc6be8e6fbb643a3e22a4ca5e3",
+    "url": "/static/js/2.94d9c751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a4dab399a20da78ee8",
+    "url": "/static/js/main.14513f60.chunk.js"
+  },
+  {
+    "revision": "739694a59e9d5a99c62e",
+    "url": "/static/js/runtime-main.6767be66.js"
+  },
+  {
+    "revision": "e4acdfa2c1c8ab733b1112afb749466d",
+    "url": "/static/media/1-MLS (10).e4acdfa2.jpg"
+  },
+  {
+    "revision": "7fdf5fc94ca17bfa5bfbfc3421d23fbb",
+    "url": "/static/media/1-dining.7fdf5fc9.jpg"
+  },
+  {
+    "revision": "b000e280ab180a127def78b5e0fdcbab",
+    "url": "/static/media/1_DSC00650.b000e280.jpg"
+  },
+  {
+    "revision": "e43699e348af0ac5eb6d90ecbcc07158",
+    "url": "/static/media/1_Dining.e43699e3.jpg"
+  },
+  {
+    "revision": "b3459d1472e9e9ad4ad350e8b379ca6b",
+    "url": "/static/media/1_Island.b3459d14.jpg"
+  },
+  {
+    "revision": "dd312e103f248b5b56528e5d504edf76",
+    "url": "/static/media/2-MLS (17).dd312e10.jpg"
+  },
+  {
+    "revision": "32182ce174ff81687a52c1cf2e678d4c",
+    "url": "/static/media/2-entry.32182ce1.jpg"
+  },
+  {
+    "revision": "2084610a55690ee436708b03b96c29f7",
+    "url": "/static/media/2_DSC00584-1080.2084610a.jpg"
+  },
+  {
+    "revision": "e097492acfc13982ad1101f8f30d6db8",
+    "url": "/static/media/2_Kitchen.e097492a.jpg"
+  },
+  {
+    "revision": "ebabca8d64970dcf197a021e0dbe7f97",
+    "url": "/static/media/2_Living.ebabca8d.jpg"
+  },
+  {
+    "revision": "af95030821a2cf31124c6511ee3b87a2",
+    "url": "/static/media/3-Guestbed4.af950308.jpg"
+  },
+  {
+    "revision": "26afb9e2a260b9d84dce1fa35c3c54da",
+    "url": "/static/media/3-MLS (33).26afb9e2.jpg"
+  },
+  {
+    "revision": "d92d480bb51d1d4987d6c8b80bb7b58c",
+    "url": "/static/media/3_Bedroom2.d92d480b.jpg"
+  },
+  {
+    "revision": "9d41d52b8f46af9d4a7941e710b4c724",
+    "url": "/static/media/3_DSC00590-1080.9d41d52b.jpg"
+  },
+  {
+    "revision": "088a60a19085534d70efd39274906535",
+    "url": "/static/media/3_Entry.088a60a1.jpg"
+  },
+  {
+    "revision": "481971bb3a35588dfef0fd67d07a96a2",
+    "url": "/static/media/4-MLS (8).481971bb.jpg"
+  },
+  {
+    "revision": "92bc8275a47ffadf3c6c64358aaba91c",
+    "url": "/static/media/4-Master3.92bc8275.jpg"
+  },
+  {
+    "revision": "5d32f7195a2ca7a90e92936d0d14433a",
+    "url": "/static/media/4_52 S MorrisonMLS.5d32f719.jpg"
+  },
+  {
+    "revision": "863af094b364a5fbd8b5836a764d1d7d",
+    "url": "/static/media/4_DSC00599-edit.863af094.jpg"
+  },
+  {
+    "revision": "051104e3f627d19c375e31c83bb9243d",
+    "url": "/static/media/4_Shed.051104e3.jpg"
+  },
+  {
+    "revision": "037ee7f0cb13c17f3bc47e8e66c28426",
+    "url": "/static/media/5-Master2.037ee7f0.jpg"
+  },
+  {
+    "revision": "595ba39491c22a009a83a5b24daae4bb",
+    "url": "/static/media/5_Bedroom.595ba394.jpg"
+  },
+  {
+    "revision": "2817e3dcf0505a81d5769422b86e481c",
+    "url": "/static/media/5_DSC00665.2817e3dc.jpg"
+  },
+  {
+    "revision": "f15754db57120db958e3e41ec00d3853",
+    "url": "/static/media/5_Kid.f15754db.jpg"
+  },
+  {
+    "revision": "21dad1e36c239d604f4487a07e53c423",
+    "url": "/static/media/6_Kid2.21dad1e3.jpg"
+  },
+  {
+    "revision": "f3fbc351b97e298e4f437eaa40a0a337",
+    "url": "/static/media/6_Living.f3fbc351.jpg"
+  },
+  {
+    "revision": "ca42f48df1659bcada347d87cdece43a",
+    "url": "/static/media/7_Breakfast_Nook.ca42f48d.jpg"
+  },
+  {
+    "revision": "127762eae093f71ecadf95d30079381f",
+    "url": "/static/media/AngelicBonquesSansItalic.127762ea.otf"
+  },
+  {
+    "revision": "956c88d5b1bea8faedb3217c9a7c54de",
+    "url": "/static/media/AngelicBonquesSansRegular.956c88d5.otf"
+  },
+  {
+    "revision": "68ab26719cb32099a4f7dbac71ec826e",
+    "url": "/static/media/AngelicBonquesScript.68ab2671.otf"
+  },
+  {
+    "revision": "22ac711171fc31def622bcb725793264",
+    "url": "/static/media/Bathroom.22ac7111.gif"
+  },
+  {
+    "revision": "e50a8d94a0201560e314f803ae1cbb88",
+    "url": "/static/media/Bathroom.e50a8d94.png"
+  },
+  {
+    "revision": "c91a9bf7775e323721c08bbfa2e65956",
+    "url": "/static/media/Bedroom1.c91a9bf7.jpg"
+  },
+  {
+    "revision": "3a670d2b49254ef1502da80de609ae42",
+    "url": "/static/media/Bedroom2.3a670d2b.jpg"
+  },
+  {
+    "revision": "9e71dee7e2105517bc0e3b022f7f6304",
+    "url": "/static/media/DiningRoom1.9e71dee7.jpg"
+  },
+  {
+    "revision": "458cdf6ce59e654583aee69ee065188a",
+    "url": "/static/media/DiningRoom2.458cdf6c.jpg"
+  },
+  {
+    "revision": "6ee6ebad1d31cbfc34c2bcc2b949b690",
+    "url": "/static/media/Kitchen.6ee6ebad.gif"
+  },
+  {
+    "revision": "e7dbfecc4fd8db499e6980df1ddfcf49",
+    "url": "/static/media/Kitchen.e7dbfecc.png"
+  },
+  {
+    "revision": "f050a088b07f54a97ed0f8467fd1fb24",
+    "url": "/static/media/Living Room1.f050a088.jpg"
+  },
+  {
+    "revision": "4ee3969a695320f577f0ba9e9ae632ed",
+    "url": "/static/media/Living Room2.4ee3969a.jpg"
+  },
+  {
+    "revision": "0b5f9600c1c9f30bebc0188387653899",
+    "url": "/static/media/Living.0b5f9600.png"
+  },
+  {
+    "revision": "2d7c076a6d3cb40afe88dfea5316813f",
+    "url": "/static/media/Living.2d7c076a.gif"
+  },
+  {
+    "revision": "58a8b6b931061ab07850d8fd2559cc32",
+    "url": "/static/media/Ty_Karges_Hse-04.58a8b6b9.jpg"
+  },
+  {
+    "revision": "f6f10ce94d0a894461139f2921606d9c",
+    "url": "/static/media/Ty_Karges_Hse-05.f6f10ce9.jpg"
+  },
+  {
+    "revision": "171d87d8a052b2d6f00446c2b6cab6be",
+    "url": "/static/media/aboutPage2.171d87d8.jpg"
+  },
+  {
+    "revision": "bc84003359165cf72ca02ea677d56840",
+    "url": "/static/media/aboutPage3.bc840033.jpg"
+  },
+  {
+    "revision": "f9be08b4e6e1db98e789cb64b1319864",
+    "url": "/static/media/contactPage1.f9be08b4.png"
+  },
+  {
+    "revision": "34eedeb1d1e95e8beae202160f630a09",
+    "url": "/static/media/homeStaging.34eedeb1.png"
+  },
+  {
+    "revision": "47acd91759690d731881dc50ccedeba8",
+    "url": "/static/media/houzzLogo.47acd917.svg"
+  },
+  {
+    "revision": "fe40dbb7c46a1dbf44d27dfea39c2936",
+    "url": "/static/media/instagramLogo.fe40dbb7.svg"
+  },
+  {
+    "revision": "b003f6374b179dc17f28cd171cb18be6",
+    "url": "/static/media/instantHome.b003f637.png"
+  },
+  {
+    "revision": "5971a2e1525b02825006fc8060a650ef",
+    "url": "/static/media/kitchen1.5971a2e1.jpg"
+  },
+  {
+    "revision": "6faa296294c2c67e5e77c62c27e57074",
+    "url": "/static/media/longArrowRight.6faa2962.svg"
+  },
+  {
+    "revision": "d4c380188ad201fc01330ed1f04c87f2",
+    "url": "/static/media/portfolio4.d4c38018.jpg"
+  },
+  {
+    "revision": "bac994aadaf3fbecf91d094464d9fb4d",
+    "url": "/static/media/portfolio5.bac994aa.jpg"
+  },
+  {
+    "revision": "57a5547369694fbf95c407bc2cab7353",
+    "url": "/static/media/servicesPage4.57a55473.png"
+  },
+  {
+    "revision": "cc10fbded6e36974d78821740d89bbf7",
+    "url": "/static/media/sloMoDogBeach.cc10fbde.mp4"
+  },
+  {
+    "revision": "dfd69de143114bd177637ee8b171de5c",
+    "url": "/static/media/threeLineMenuIcon.dfd69de1.svg"
+  },
+  {
+    "revision": "932748bd48553c032b4d2cc7b7545a71",
+    "url": "/static/media/tykargesLogo.932748bd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfc587eadc3b604b307d4ff8d9dbf974.js"
+  "/precache-manifest.c51cdffb8a9f2285650b9bc38019059a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
