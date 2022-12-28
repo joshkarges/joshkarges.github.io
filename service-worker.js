ec96317d1d980ca5ae935a3c191bbbc5823da6bd
Josh Karges <joshkarges@gmail.com>
2022-12-27 22:35:46 -0800

    Deploy 1672209344021

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d36e90af8996e4eb57df709f7cd5afc9.js"
+  "/precache-manifest.5d5cc449b74be4f8661bc5e206757be1.js"
 );
 
 self.addEventListener('message', (event) => {
